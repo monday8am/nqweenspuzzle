5f78b15f0de3bd87309af7d1737bb32b0c5c1cc7
Anton <hello@monday8am.com>
2025-12-22 00:48:13 +0100

    Set java used to 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,11 +27,14 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
         compose = true
+    }
+    kotlin {
+        jvmToolchain(17)
     }
 
     testOptions {
